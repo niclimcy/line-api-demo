16a410ebbfbbb6553a36f3e33010e28d0d4cbbb0
Nicholas Lim <nicholas.l@cloudnctech.com>
2024-05-17 15:21:38 +0800

    src: ws: Implement session handling with chat

--- src/ws.ts
+++ src/ws.ts
@@ -1,20 +1,31 @@
 import { Server as WSServer } from 'socket.io'
 import type { Server } from 'node:http'
+import { getSession } from '@auth/express'
+import { Request } from 'express'
+import { authConfig } from './config/auth.config'
+
+function mapToExpressReq(request: any) {
+  return { headers: request.headers, protocol: 'http' }
+}
 
 export function handleWS(httpServer: Server) {
   const io = new WSServer(httpServer, {
     /* options */
   })
 
-  io.on('connection', (socket) => {
-    io.emit('chat message', socket.id + ' connected')
+  io.on('connection', async (socket) => {
+    const req = mapToExpressReq(socket.request) as Request
+    const session = await getSession(req, authConfig)
+    const userName = session?.user?.name ?? socket.id
+
+    io.emit('chat message', userName + ' connected')
 
     socket.on('disconnect', () => {
-      io.emit('chat message', socket.id + ' disconnected')
+      io.emit('chat message', userName + ' disconnected')
     })
 
     socket.on('chat message', (msg) => {
-      io.emit('chat message', `${socket.id}: ${msg}`)
+      io.emit('chat message', `${userName}: ${msg}`)
     })
   })
 }
