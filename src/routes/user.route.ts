b31cdaccae42b21a47f78c097c80158319016fe1
Nicholas Lim <nicholas.l@cloudnctech.com>
2024-05-20 15:44:31 +0800

    Replace @json2csv/plainjs with papaparse

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,13 +1,13 @@
 import { Router, type Request, type Response } from 'express'
 import { authorizationRequired } from '../middleware/auth.middleware'
 import User from '../schemas/user.schema'
-import { Parser } from '@json2csv/plainjs'
+import Papa from 'papaparse'
 
 const router = Router()
 
 const getUsers = async (req: Request, res: Response) => {
   try {
-    const users = await User.find({})
+    const users = await User.find({}).lean()
     return users
   } catch (error) {
     return res.status(500).json({ error: 'Failed to fetch users' })
@@ -37,18 +37,16 @@ router.get(
         return users
       }
 
-      const fields = [
-        { label: 'Name', value: 'name' },
-        { label: 'Email', value: 'email' },
-        { label: 'Address', value: 'address' },
-        { label: 'Registered', value: 'registered' },
-      ]
+      if (!Array.isArray(users)) {
+        throw new Error('Expected an array of users')
+      }
 
-      const parser = new Parser({ fields })
-      const csv = parser.parse(users)
+      const usersCsv = Papa.unparse(users, {
+        columns: ['name', 'email', 'address', 'emailVerified'],
+      })
 
       res.setHeader('Content-Type', 'text/csv')
-      res.attachment('users.csv').send(csv)
+      res.attachment('users.csv').send(usersCsv)
     } catch (error) {
       console.log(error)
       return res.status(500).json({ error: 'Failed to export users' })
