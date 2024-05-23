27a35af63e395121b6b80916639ced61cae5bfbd
Nicholas Lim <nicholas.l@cloudnctech.com>
2024-05-23 18:14:47 +0800

    Fix production build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,9 @@
 import type { Config } from 'tailwindcss'
 
-const config = {
+export default {
   content: ['./src/**/*.{ts,ejs}'],
   theme: {
     extend: {},
   },
   plugins: [],
 } satisfies Config
-
-export default config
