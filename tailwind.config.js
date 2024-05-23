2480778e9c6137c45d60435467890bbfe188e57b
Nicholas Lim <nicholas.l@cloudnctech.com>
2024-05-23 17:56:51 +0800

    Remove remaining js configs

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,css,ejs}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss'
+
+const config = {
+  content: ['./src/**/*.{ts,ejs}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
+export default config
