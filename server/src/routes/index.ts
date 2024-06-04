45707a064dbae9ea9dd65d2c7b1f04f71bb2d3ad
Nicholas Lim <nicholas.l@cloudnctech.com>
2024-06-04 14:15:52 +0700

    server: Remove unused ejs templates

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,44 +1,10 @@
 import { Router, type Request, type Response } from 'express'
-import { authenticatedUser } from '../middleware/auth.middleware'
-import User from '../schemas/user.schema'
 
 const router = Router()
-
-router.get('/', authenticatedUser, async (req: Request, res: Response) => {
-  {
-    if (res.locals.session && res.locals.session.user.email) {
-      const email = res.locals.session.user.email
-
-      try {
-        // Find the user by email
-        const user = await User.findOne({ email })
-
-        // Check if the user is registered
-        if (user && !user.registered) {
-          // If the user is not registered, redirect to the registration page
-          return res.redirect('/auth/register')
-        }
-      } catch (error) {
-        console.log(error)
-        return res.render('home', { session: res.locals?.session })
-      }
-    }
-
-    return res.render('home', { session: res.locals?.session })
-  }
-})
 
 router.get('/auth/signup', (req: Request, res: Response) => {
   {
     return res.render('signup', { error: '' })
-  }
-})
-
-router.get('/chat', authenticatedUser, async (req: Request, res: Response) => {
-  {
-    if (!res.locals?.session) return res.redirect('/')
-
-    return res.render('chat')
   }
 })
 
